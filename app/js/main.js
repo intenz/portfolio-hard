5bde455cbcdeab9f80bcecb6c2b07ab7e89da7d6
Viktor <_intenz_@ukr.net>
2015-10-03 17:06:38 +0300

    two three page

--- app/js/main.js
+++ app/js/main.js
@@ -4,3 +4,49 @@ $(document).ready(function() {
 		$('.nav_menu').toggleClass('active');
 	});
 });
+
+
+// popup
+var myWork = (function (){
+
+var init = function(){
+		_setUpListners();
+	},
+	_setUpListners = function (){
+	$('#add_form').on('click', _showModal);
+	$('#enter').on('click', _showModal); // открыть модальное окно
+	('#add_work_form').on('submit', _addProject); // добавление проекта
+},
+
+_showModal = function (){
+		$('#element_to_pop_up').bPopup({
+			onClose: function () {
+	this.find('.form').trigger("reset"); // сбрасываем форму
+}
+		});
+	},
+
+_addProject = function (e){
+
+	e.preventDefault();
+		var form = $(this),
+			url = '/send_mail.php',
+			defObject = _ajaxForm(form, url);
+},
+
+_ajaxForm = function (form, url) {
+
+	if (!validation.validateForm(form)) return false;  
+	var data = form.serialize();
+
+
+};
+
+return {
+		init: init
+};
+
+})();
+
+myWork.init();
+jQuery('input[placeholder], textarea[placeholder]').placeholder();
