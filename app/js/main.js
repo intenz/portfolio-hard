3574d5e57c3b015316dd5caa882b4938231bb7bf
Viktor <_intenz_@ukr.net>
2015-10-05 19:51:02 +0300

    add prog

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,5 @@
+
+// menu
 $(document).ready(function() {
 	$('.menu_show').click(function(e) {
 		$(this).toggleClass('active');
@@ -46,3 +48,16 @@ return {
 
 myWork.init();
 jQuery('input[placeholder], textarea[placeholder]').placeholder();
+
+// file-load
+function getName (str){
+	if (str.lastIndexOf('\\')){
+	    var i = str.lastIndexOf('\\')+1;
+	}
+	else{
+	    var i = str.lastIndexOf('/')+1;
+	}						
+	var filename = str.slice(i);			
+	var uploaded = document.getElementById("fileformlabel");
+	uploaded.innerHTML = filename;
+}
